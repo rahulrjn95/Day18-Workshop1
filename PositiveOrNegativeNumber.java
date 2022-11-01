19bb7844b917f0a55ae8844ef74ff1f703375e73
rahulrjn95 <rahulrjn357@gmail.com>
2022-11-01 21:49:15 +0530

    added positive or negative

---
+++ PositiveOrNegativeNumber.java
@@ -0,0 +1,21 @@
+package com.bridgelabz;
+
+// we have to write a program for Positive Or Negative Number
+
+public class PositiveOrNegativeNumber {
+    public static void main(String[] args) {
+
+        int num = -250;
+
+        if (num > 0) {
+            System.out.println("The number is positive");
+        }
+        if (num < 0) {
+            System.out.println("The number is negative");
+        }
+        if (num == 0)
+        {
+            System.out.println("The number is Zero");
+        }
+    }
+}
